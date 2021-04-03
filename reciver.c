4176c84aee96e2df0e3fe212831899372b3334a4
user <user@mail.example.com>
2021-04-03 23:02:25 +0200

    revicer wip

--- reciver.c
+++ reciver.c
@@ -2,20 +2,25 @@
 
 int recive_icmp_packets(int sockfd, int pid, int seq_start, int num_of_packets, int wait_time_in_seconds){
 
-	fd_set descriptors;
-	int responses = 0;
+	clock_t start;
+	start = clock();
+	int responses = 0; // number_of_recived packets
+	clock_t timestamps[num_of_packets]; // times of packets reciving 	
+	char ip[num_of_packets][30]; // storage of ips	
+	// Select setup
 	int select_status;
-	struct timeval tv; tv.tv_sec = wait_time_in_seconds; tv.tv_usec = 0;
+	struct timeval tv; tv.tv_sec = wait_time_in_seconds; tv.tv_usec = 0; // timeout time
+	fd_set descriptors;
+	FD_ZERO(&descriptors);
+	FD_SET(sockfd, &descriptors);
+	// recive setup
 	struct sockaddr_in sender;
 	socklen_t sender_len =sizeof(sender);
 	u_int8_t buffer[IP_MAXPACKET];
-
-	FD_ZERO(&descriptors);
-	FD_SET(sockfd, &descriptors);
-
+	// Packet unpacking variables
 	struct ip* ip_header;
 	struct icmp* icmp_header;
-	struct in_addr* r_ip;
+	struct in_addr* response_ip;
 	
 	while (responses < num_of_packets){
 		select_status = select(sockfd+1, &descriptors, NULL, NULL, &tv);
@@ -23,44 +28,36 @@ int recive_icmp_packets(int sockfd, int pid, int seq_start, int num_of_packets, 
 			return -1;
 		}
 		else if (select_status == 0){			
-			return responses;
+			break;
 		}
 		else{
 			recvfrom(sockfd, buffer, IP_MAXPACKET, MSG_DONTWAIT, (struct sockaddr*)&sender, &sender_len);
 
 			ip_header = (struct ip*) buffer;
 			icmp_header = (struct icmp*) (buffer + 4 * ip_header->ip_hl);
-			r_ip = &ip_header -> ip_src;
+			response_ip = &ip_header -> ip_src;
 			
-			
-			if (icmp_header -> icmp_type == ICMP_TIME_EXCEEDED){
-				
-				ip_header = (struct ip *)((uint8_t *) icmp_header+8);
-				icmp_header = (struct icmp *) ((uint8_t *) ip_header + ip_header->ip_hl * 4);
+			if (icmp_header -> icmp_type == ICMP_TIME_EXCEEDED || icmp_header -> icmp_type == ICMP_ECHOREPLY){
+				if (icmp_header -> icmp_type == ICMP_TIME_EXCEEDED){
+					ip_header = (struct ip *)((uint8_t *)icmp_header + 8);			
+					icmp_header = (struct icmp *) ((uint8_t *) ip_header + ip_header->ip_hl * 4);
+				}
 				
 				if (seq_start <= icmp_header -> icmp_seq && 
 								icmp_header -> icmp_seq  <= seq_start+num_of_packets &&
 								icmp_header -> icmp_id==pid){
-
-					char ip_1[30];
-					inet_ntop(AF_INET, r_ip, ip_1, sizeof(ip_1));
-					printf("%s", ip_1);
-					responses++;		
-					return responses;
+					inet_ntop(AF_INET, response_ip, ip[responses], sizeof(char[30]));
+					timestamps[responses] = clock();
+					responses++;
 				}			
-			}
-			else if (icmp_header -> icmp_type == ICMP_ECHOREPLY && 
-				seq_start <= icmp_header -> icmp_seq && 
-				icmp_header -> icmp_seq  <= seq_start+num_of_packets &&
-				icmp_header -> icmp_id==pid){
-					char ip_1[30];
-					inet_ntop(AF_INET, r_ip, ip_1, sizeof(ip_1));
-					printf("%s", ip_1);
-					responses++;		
-					return responses;	
-				}
-
+			}	
 		}
 	}
-	return responses;
+
+	for (int i = 0; i<responses; i++){
+			int msec = (int)(timestamps[i]-start)/CLOCKS_PER_SEC*1000;
+			printf("%s %d", ip[i], msec % 1000);
+	}
+
+	return 1;
 }
--- reciver.h
+++ reciver.h
@@ -7,7 +7,6 @@
 #include <time.h>
 #include <netinet/in.h>
 #include <stdio.h>
-#include <string.h>
 
 
 int recive_icmp_packets(int sockfd, int pid, int seq_start, int num_of_packets, int wait_time_in_seconds);
